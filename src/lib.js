a2f25b7e999dc1bf286cdbc1a6427301fc2d9745
Kieran Simpson <kierans777@gmail.com>
2015-06-05 21:42:48 +1000

    Implemented library.

--- src/lib.js
+++ src/lib.js
@@ -1,1 +1,192 @@
 "use strict";
+
+var stream = require("stream"),
+    PassThrough = stream.PassThrough;
+
+var clone = require("clone"),
+    sax = require("sax");
+
+Array.prototype.top = function() { // jshint ignore:line
+  return this[this.length - 1];
+};
+
+var ENTITY_REGEXP = new RegExp("<!ENTITY (\\w+) \"(.*?)\">", "g");
+
+function SAXStreamer(opts) {
+  this.opts = opts || {};
+  this.opts.formatting = this.opts.formatting || {};
+
+  var formatting = this.opts.formatting;
+  formatting.spaceBeforeSelfClosingTag = formatting.spaceBeforeSelfClosingTag !== undefined ? formatting.spaceBeforeSelfClosingTag : true;
+}
+
+module.exports = SAXStreamer;
+
+/**
+ * Pipes src through the sax module.  Other args are passed to sax.createStream.
+ *
+ * Attaches default event listeners.
+ *
+ * @param {Readable} src
+ * @param {boolean} strict
+ * @param [options]
+ * @return {PassThrough} dest stream
+ */
+SAXStreamer.prototype.createStream = function(src, strict, options) {
+  options = options || {};
+  options.strictEntities = options.strictEntities || true;
+
+  var dest = new PassThrough();
+  dest.setEncoding("utf8");
+
+  dest.on("drain", function () {
+    src.resume();
+  });
+
+  var saxStream = sax.createStream(strict, options);
+  saxStream.print = function() {
+    var args = Array.prototype.slice.apply(arguments);
+
+    if (!dest.write.apply(dest, args)) {
+      src.pause();
+    }
+  };
+
+  saxStream.encodeEntities = encodeEntities.bind(saxStream);
+
+  saxStream.tags = [];
+  saxStream.opts = this.opts;
+  // maps entity value to entity key eg: " => &quot;
+  saxStream.ents = {};
+
+  saxStream.on("text", this.text);
+  saxStream.on("doctype", this.doctype);
+  saxStream.on("opentag", this.opentag);
+  saxStream.on("closetag", this.closetag);
+  saxStream.on("processinginstruction", this.processinginstruction);
+  saxStream.on("opencdata", this.opencdata);
+  saxStream.on("cdata", this.cdata);
+  saxStream.on("closecdata", this.closecdata);
+  saxStream.on("comment", this.comment);
+  saxStream.on("error", this.error);
+  saxStream.on("end", function() {
+    dest.end();
+  });
+
+  setEntities(this, saxStream);
+
+  src.pipe(saxStream);
+
+  return dest;
+};
+
+SAXStreamer.prototype.identity = function(text) {
+  this.print(this.encodeEntities(text));
+};
+
+SAXStreamer.prototype.text = SAXStreamer.prototype.identity;
+SAXStreamer.prototype.doctype = function(doctype) {
+  parseEntities(this, doctype);
+
+  this.print("<!DOCTYPE");
+  this.print(doctype);
+  this.print(">");
+};
+
+SAXStreamer.prototype.opentag = function (tag) {
+  this.print("<" + tag.name);
+
+  for (var i in tag.attributes) {
+    if (tag.attributes.hasOwnProperty(i)) {
+      this.print(" " + i + "=\""+ this.encodeEntities(tag.attributes[i]) + "\"");
+    }
+  }
+
+  if (tag.isSelfClosing) {
+    if (this.opts.formatting.spaceBeforeSelfClosingTag) {
+      this.print(" ");
+    }
+
+    this.print("/");
+  }
+
+  this.print(">");
+
+  this.tags.push(tag);
+};
+
+SAXStreamer.prototype.closetag = function (name) {
+  if (!this.tags.top().isSelfClosing) {
+    this.print("</" + name + ">");
+  }
+
+  this.tags.pop();
+};
+
+SAXStreamer.prototype.processinginstruction = function(instruction) {
+  this.print("<?");
+  this.print(instruction.name);
+  this.print(" ");
+
+  if (instruction.body) {
+    this.print(instruction.body.replace(new RegExp(" = ", "g"), "=").replace(new RegExp("'", "g"), "\""));
+  }
+
+  this.print("?>");
+};
+
+SAXStreamer.prototype.opencdata = function() {
+  this.print("<![CDATA[");
+};
+
+SAXStreamer.prototype.cdata = function (data) {
+  this.print(data);
+};
+
+SAXStreamer.prototype.closecdata = function() {
+  this.print("]]>");
+};
+
+SAXStreamer.prototype.comment = function (comment) {
+  this.print("<!--" + comment + "-->");
+};
+
+SAXStreamer.prototype.error = function(err) {
+  throw err;
+};
+
+function setEntities(saxStreamer, saxStream) {
+  if (saxStreamer.opts.entities) {
+    saxStream._parser.ENTITIES = saxStreamer.opts.entities;
+  }
+  else {
+    // For some reason the ENTITIES aren't being created properly with Object.create().
+    saxStream._parser.ENTITIES = saxStream._parser.strictEntities ? clone(sax.XML_ENTITIES) : clone(sax.ENTITIES);
+  }
+
+  Object.keys(saxStream._parser.ENTITIES).forEach(function(entity) {
+    saxStream.ents[saxStream._parser.ENTITIES[entity]] = entity;
+  });
+}
+
+function encodeEntities(str) {
+  var ents = this.ents; // jshint ignore:line
+
+  Object.keys(ents).forEach(function(entity) {
+    str = str.replace(new RegExp(entity, "g"), "&" + ents[entity] + ";");
+  });
+
+  return str;
+}
+
+function parseEntities(saxStream, doctype) {
+  var entities;
+  do {
+    entities = ENTITY_REGEXP.exec(doctype);
+
+    if (entities) {
+      saxStream._parser.ENTITIES[entities[1]] = entities[2];
+    }
+  }
+  while(entities);
+}
---
+++ test/sax-streamer-test.js
@@ -0,0 +1,125 @@
+/*jshint expr: true*/
+
+"use strict";
+
+var fs = require("fs"),
+    path = require("path");
+
+var chai = require("chai"),
+    expect = chai.expect,
+    should = require("mocha-should");
+
+var clone = require("clone"),
+    concat = require("concat-stream"),
+    glob = require("glob"),
+    nimble = require("nimble"),
+    sax = require("sax");
+
+var SAXStreamer = require("../src/lib");
+
+describe("SAX Streamer tests", function() {
+  var opts, expectedFile;
+
+  function testFiles(done) {
+    return function(err, files) {
+      if (err) {
+        return done(err);
+      }
+
+      nimble.series(files.map(function(filename) {
+        return function(next) {
+          //console.log("Testing '" + filename + "'");
+
+          var file = createFileReadStream(filename, next);
+
+          var expected, result;
+
+          nimble.parallel([
+            // load the expected representation
+            function(cb) {
+              createFileReadStream(expectedFile(filename), cb).pipe(concat(function(data) {
+                expected = data;
+
+                cb();
+              }));
+            },
+
+            // stream the file through the parser
+            function(cb) {
+              var saxStreamer = new SAXStreamer(opts);
+              saxStreamer.error = function(err) {
+                cb(err);
+              };
+
+              saxStreamer.createStream(file, true, {}).pipe(concat(function(data) {
+                result = data;
+
+                cb();
+              }));
+            }
+          ], function(err) {
+            if (err) {
+              return next(err);
+            }
+
+            expect(result.trim(), "Output from file '" + filename + "' not correct").to.equal(expected);
+
+            next();
+          });
+        };
+      }), done);
+    };
+  }
+
+  beforeEach(function() {
+    expectedFile = null;
+    opts = null;
+  });
+
+  should("output identical copy of input", function(done) {
+    expectedFile = function(filename) {
+      return path.dirname(filename) + "/expected/defaults/" + path.basename(filename);
+    };
+
+    glob("test/files/*.xml", testFiles(done));
+  });
+
+  should("not use space before self closing tag when formatting not set", function(done) {
+    opts = {
+      formatting: {
+        spaceBeforeSelfClosingTag: false
+      }
+    };
+
+    expectedFile = function(filename) {
+      return path.dirname(filename) + "/expected/nospace-self-closing-tags.xml";
+    };
+
+    glob("test/files/formatting.xml", testFiles(done));
+  });
+
+  should("use entities provided", function(done) {
+    var entities = clone(sax.XML_ENTITIES);
+    delete entities.apos;
+
+    opts = {
+      entities: entities
+    };
+
+    expectedFile = function(filename) {
+      return path.dirname(filename) + "/expected/xml-entities-provided-entities.xml";
+    };
+
+    glob("test/files/xml-entities.xml", testFiles(done));
+  });
+});
+
+function createFileReadStream(filename, cb) {
+  var file = fs.createReadStream(filename);
+  file.setEncoding("utf8");
+  file.on("error", function(err) {
+    cb(err);
+  });
+
+  return file;
+}
